5ec42a4322cc9933d89d3b8d713623cf67dc32d5
aspsa <123aspsa123@gmail.com>
2016-01-22 20:27:23 +0000

    Reset.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-
-  get 'welcome/about'
   
   root to: 'welcome#index'
+
+  #get 'welcome/index'
+  get 'welcome/about'
+  
+  resources :registered_applications
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
